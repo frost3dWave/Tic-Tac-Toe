d594abe874590532df9d850492d622ee859e9e55
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2025-03-28 19:39:55 +0530

    updated the playerController & gameController func
    change to playerController ensures that players are getting switched each turn
    updated the gameController to display & store the playerControl values onto the board

--- main.js
+++ main.js
@@ -1,138 +1,3 @@
-// function gameBoard() {
-//     const cells = document.querySelectorAll(".cell");
-//     const board = [];
-//     const rows = 3;
-//     const columns = 3;
-
-//     for (let i = 0; i < rows; i++) {
-//         board[i] = [];
-//         for (let j = 0; j < columns; j++) {
-//             board[i][j] = "";
-//         };
-//     };
-
-//     // for placing the marker & updating the board 
-//     function placingMarker(row, column, marker) {
-//         if (board[row][column] === "") { // but only if the current selected element in array is an empty string i.e has nothing inside
-//             board[row][column] = marker;
-//             return true;
-//         }else {
-//             console.log("position already taken! Please enter again.");
-//             return false;
-//         };
-//     };
-
-//     const getBoard = () => board;  
-
-//     // check if board is full 
-//     function isBoardFull() {
-//         for (let i=0; i<rows; i++){
-//             for (j=0; j<columns; j++){
-//                 if (board[i][j] === ""){
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     };
-
-//     // to reset the board 
-//     function resetBoard() {
-//         for (let i = 0; i < rows; i++){
-//             for (let j = 0; j < columns; j++) {
-//                 board[i][j] = "";
-//             }
-//         } 
-//     };
-
-//     return {getBoard, resetBoard, isBoardFull, placingMarker};
-// }
-
-// function playerController() {
-//     const players = [
-//         {
-//             name: "playerOne",
-//             marker: "X"
-//         }, 
-//         {
-//             name: "playerTwo",
-//             marker: "O"
-//         }
-//     ];
-
-//     let activePlayer = players[0];
-
-//     function switchPlayerTurn() {
-//         activePlayer = (activePlayer === players[0]) ? players[1] : players[0];
-//     };
-
-//     const getActivePlayer = () => activePlayer;
-
-//     return {switchPlayerTurn, getActivePlayer};
-// }
-
-// function gameController() {
-//     const board = gameBoard();
-//     const players = playerController();
-    
-//     function playGame() {
-//         console.log("Game started!");
-//         let gameOver = false;
-        
-//         while (!gameOver) {
-//             playRound();
-//             // Check for win or tie
-//             if (checkForWin() || board.isBoardFull()) {
-//                 gameOver = true;
-//             }
-//         }
-        
-//         announceResult();
-//     }
-    
-//     function playRound() {
-//         const currentPlayer = players.getActivePlayer();
-//         let validMove = false;
-        
-//         while (!validMove) {
-//             let rowChoice = prompt("Enter row (0-2): ");
-//             let columnChoice = prompt("Enter column (0-2): ");
-            
-//             validMove = board.placingMarker(rowChoice, columnChoice, currentPlayer.marker);
-            
-//             if (!validMove) {
-//                 console.log("Invalid move! Try again.");
-//             }
-//         }
-        
-//         // After a valid move, switch player
-//         players.switchPlayerTurn();
-//     }
-    
-//     function checkForWin() {
-//         // You'll need to implement win condition checks
-//         // Check rows, columns, and diagonals
-//         return false; // Placeholder
-//     }
-    
-//     function announceResult() {
-//         if (checkForWin()) {
-//             // Since we switched players after the last move, 
-//             // we need to switch back to get the winner
-//             players.switchPlayerTurn();
-//             console.log(`${players.getActivePlayer().name} wins!`);
-//         } else if (board.isBoardFull()) {
-//             console.log("It's a tie!");
-//         }
-//     }
-    
-//     function restart() {
-//         board.resetBoard();
-//         // Reset to first player if needed
-//     }
-    
-//     return { playGame, restart};
-// }
 let running = false;
 gameController();
 
@@ -157,16 +22,14 @@ function gameBoard(){
     }
 
     function updateCell(cell, row, column, marker){
-        console.log(board);
         board[row][column] = marker;
         cell.textContent = marker;
     }
-    return {cellClicked, updateCell};
+    return {board, cellClicked, updateCell};
 };
 
 function initializeGame(){
     const cells = document.querySelectorAll(".cell");
-    // cells.forEach(cell => cell.addEventListener("click", cellClicked));
 
     const restartBtn = document.querySelector(".restartBtn");
     // restartBtn.addEventListener("click", restartGame);
@@ -178,34 +41,93 @@ function initializeGame(){
     return {cells, restartBtn, gameStatus, running};
 };
 
-function playerController(){
+function playerController(gameInitialize) {
     const players = [
-        {
-            marker : "X",
-        },
-        {
-            marker : "O",
-        },
-    ]
+        { marker: "X" },
+        { marker: "O" }
+    ];
 
-    const activePlayer = players[0].marker;
+    let activePlayerIndex = 0;
 
-    function changeActivePlayer(){
-        activePlayer = (activePlayer === players[0].marker) ? players[1].marker : players[0].marker; 
+    function getActivePlayer() {
+        return players[activePlayerIndex].marker;
     }
 
-    return {activePlayer, changeActivePlayer};
-};
+    function changeActivePlayer() {
+        activePlayerIndex = activePlayerIndex === 0 ? 1 : 0;
+        gameInitialize.gameStatus.textContent = `${getActivePlayer()}'s turn`;
+        return getActivePlayer();
+    }
+
+    return {
+        get activePlayer() {
+            return getActivePlayer();
+        },
+        changeActivePlayer
+    };
+}
 
 function gameController(){
     running = true;
     const gameInitialize = initializeGame();
     const boardGame = gameBoard();
-    const playercontrol = playerController();
+    const playercontrol = playerController(gameInitialize);
 
     gameInitialize.gameStatus.textContent = `${playercontrol.activePlayer}'s turn`;
-    gameInitialize.cells.forEach(cell => 
-        cell.addEventListener("click", (e) => {
-            boardGame.cellClicked(e.target, playercontrol.activePlayer)
-        }));
-}
+    
+    gameInitialize.cells.forEach(cell => cell.addEventListener("click", (e) => { 
+        
+        boardGame.cellClicked(e.target, playercontrol.activePlayer);
+        
+        checkWinner();
+    }));
+
+    function checkWinner(){
+        const winConditions = [
+            // Rows
+            [[0,0], [0,1], [0,2]],
+            [[1,0], [1,1], [1,2]],
+            [[2,0], [2,1], [2,2]],
+            
+            // Columns
+            [[0,0], [1,0], [2,0]],
+            [[0,1], [1,1], [2,1]],
+            [[0,2], [1,2], [2,2]],
+            
+            // Diagonals
+            [[0,0], [1,1], [2,2]],
+            [[0,2], [1,1], [2,0]]
+        ];
+    
+        let roundWon = false;
+        for(let condition of winConditions){
+            const [a, b, c] = condition;
+            const cellA = boardGame.board[a[0]][a[1]];
+            const cellB = boardGame.board[b[0]][b[1]];
+            const cellC = boardGame.board[c[0]][c[1]];
+    
+            // Check if cells are not empty and match
+            if(cellA && cellA === cellB && cellB === cellC){
+                // console.log("Win condition found!");
+                roundWon = true;
+                break;
+            }
+        }
+    
+        // Check for board fullness (draw)
+        const isBoardFull = boardGame.board.every(row => row.every(cell => cell !== ""));
+    
+    
+        if (roundWon){
+            gameInitialize.gameStatus.textContent = `${playercontrol.activePlayer} wins!`;
+            running = false;
+        }
+        else if(isBoardFull){
+            gameInitialize.gameStatus.textContent = `Draw!`;
+            running = false;
+        }
+        else {
+            playercontrol.changeActivePlayer();
+        }
+    }
+};
